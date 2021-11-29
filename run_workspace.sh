3a2f8b80ae7e6433af7f9653dee872c1a65e83f5
Kurotora <kurotora.kun@gmail.com>
2021-11-29 01:13:59 -0800

    Deploy script development

--- run_workspace.sh
+++ run_workspace.sh
@@ -3,6 +3,12 @@
 # Workspace name: Virtualization and Orchestration as a Mean to Deploy an Infrastructure Test Environment
 # Acronym: VOMDITE
 
+# [ STEPS ]
+
+# 1.- Create workspace-home directory with permissions as 777
+mkdir --mode=777 workspace-home
+
+# 2.- Create docker container
 docker create --name spaceVOMDITE \
   -p 8020-8035:8020-8035 \
   -p 9000:9000 \
@@ -10,4 +16,12 @@ docker create --name spaceVOMDITE \
   -v $(pwd)/static-server/:/home/static-server/ \
   --restart unless-stopped \
   alnoda/ansible-terraform-workspace
+
+docker start spaceVOMDITE
+
+# .- Wait for container being up&running
+echo "Waiting container to be loaded..."
+sleep 30
+
+# .- Generate ssh keys on ./VOMDITE/terraform-files/ssh_keys/
 
